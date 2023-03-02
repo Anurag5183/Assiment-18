38d4ccc5a612983a0f6871ae35423193913e66a6
Anurag5183 <anuragsuthar2002@gmail.com>
2023-03-02 17:19:09 +0530

    New version

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function equal() {
 function c() {
   document.form.textView.value = " ";
 }
+function darkmode() {
+  var body = document.body;
+  body.classList.toggle("darkmode");
+
+  var mode = document.getElementById("dark mode").textContent= 'Dark-Mode';
+  if (mode.textContent == 'Dark-Mode') {
+    mode.textContent = "Light Mode";
+  } else {
+    mode.target.value("Dark Mode");
+  }
+}
